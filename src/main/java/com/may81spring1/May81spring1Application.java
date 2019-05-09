050c0ff6af3d795894c8a0963f5ff57f6669afd9
Matt Cadorette <mcadorette@gmail.com>
2019-05-08 20:43:55 -0700

    Initial commit from Atomist
    [atomist:generated] [atomist:generator=create-spring]

---
+++ src/main/java/com/may81spring1/May81spring1Application.java
@@ -0,0 +1,12 @@
+package com.may81spring1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class May81spring1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(May81spring1Application.class, args);
+	}
+}
